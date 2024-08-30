20b28224fdc58a10020e8f7e89b850dd0d41bed1
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-08-29 22:49:09 -0500

    arreglando likes

--- src/app/components/PostMainLikes.tsx
+++ src/app/components/PostMainLikes.tsx
@@ -5,10 +5,15 @@ import { CiHeart } from 'react-icons/ci';
 
 const PostMainLikes = ({ post }: PostMainCompTypes) => {
 
-    const [likedClick, setLikedClick] = useState<boolean>(true);
+    const [likedClick, setLikedClick] = useState<boolean>(false);
 
     const handleLike = () => {
         setLikedClick(!likedClick)
+        const likes = document.getElementById('likes');
+        const likess = likes ? parseInt(likes.innerHTML) : 0;
+        if (likes) {
+            likes.innerHTML = (likess + 1).toString();
+        }
     }
 
     return (
@@ -19,6 +24,7 @@ const PostMainLikes = ({ post }: PostMainCompTypes) => {
                     {
                         likedClick ? (<AiFillHeart  color={post.likes?.length > 0 ? '#ff2626' : 'gray'} size={30} />) : ( <CiHeart size={30}/>)
                     }
+                    <span className='text-white' id='likes'>{post?.likes.length}</span>
                     </button>
                 </div>
             </div>
